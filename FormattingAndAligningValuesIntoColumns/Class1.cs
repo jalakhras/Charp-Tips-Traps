a0ebd8d66685503a1e8aca7a526e22cd0cb89c3a
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-05-23 00:09:43 +0300

    Formatting and aligning values into columns

---
+++ FormattingAndAligningValuesIntoColumns/Class1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace FormattingAndAligningValuesIntoColumns
+{
+    [TestClass]
+    public class FormattingColumnsExample
+    {
+        [TestMethod]
+        public void TheHardWay()
+        {
+            const string name = "Claire";
+            const int age = 30;
+            const string gender = "female";
+
+            var output = "Name: " + name.PadRight(20) +
+                         "Age: " + age.ToString().PadLeft(10) +
+                         "Gender: " + gender.PadRight(20);
+
+            Debug.WriteLine(output);
+        }
+
+        [TestMethod]
+        public void TheEasierWay()
+        {
+            const string name = "Claire";
+            const int age = 30;
+            const string gender = "female";
+            var output = string.Format("Name: {0,-20}Age: {1,-10}Gender: {2,-20}",
+            name, age, gender);
+            Debug.WriteLine(output);
+            Debug.WriteLine("Name: {0,-20}Age: {1,10}Gender: {2,-20}",
+                                                        name, age, gender);
+        }
+
+    }
+}
