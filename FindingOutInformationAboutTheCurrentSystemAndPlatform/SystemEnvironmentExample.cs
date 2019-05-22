4f438ee4bd1324ed1ecd9c7c490a3137bf547e6b
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-05-23 00:24:11 +0300

    Finding out information about the current system and platform

---
+++ FindingOutInformationAboutTheCurrentSystemAndPlatform/SystemEnvironmentExample.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindingOutInformationAboutTheCurrentSystemAndPlatform
+{
+    [TestClass]
+    public class SystemEnvironmentExample
+    {
+
+        [TestMethod]
+        public void SystemRelatedInfo()
+        {
+            var envVars = System.Environment.GetEnvironmentVariables();
+            var envPathVar = envVars["Path"];
+
+            var is64BitOs = Environment.Is64BitOperatingSystem;
+            var is64BitProc = Environment.Is64BitProcess;
+            var numberOfProcs = Environment.ProcessorCount;
+            var pageSize = Environment.SystemPageSize;
+            var clrRuntimeVersion = Environment.Version;
+            var currEnvNewLineString = Environment.NewLine;
+        }
+
+
+        [TestMethod]
+        public void DirectoryRelatedInfo()
+        {
+            var currentWorkingDir = Environment.CurrentDirectory;
+
+            var desktopDir = Environment.GetFolderPath(
+                Environment.SpecialFolder.DesktopDirectory);
+
+            var fontsDir = Environment.GetFolderPath(
+                Environment.SpecialFolder.Fonts);
+
+            var myDocsDir = Environment.GetFolderPath(
+                Environment.SpecialFolder.MyDocuments);
+
+            var usersStartupDir = Environment.GetFolderPath(
+                Environment.SpecialFolder.Startup);
+
+            var drives = Environment.GetLogicalDrives();
+
+            var sysDir = Environment.SystemDirectory;
+        }
+
+
+    }
+}
