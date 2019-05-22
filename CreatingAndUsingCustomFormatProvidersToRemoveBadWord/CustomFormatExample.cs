3070b7504063eb910e2e7126becbc1a009bcf871
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-05-22 23:53:34 +0300

    Creating and using custom format providers to remive bad words

---
+++ CreatingAndUsingCustomFormatProvidersToRemoveBadWord/CustomFormatExample.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SupportCode;
+
+namespace CreatingAndUsingCustomFormatProvidersToRemoveBadWord
+{
+    [TestClass]
+    public class CustomFormatExample
+    {
+
+        [TestMethod]
+        public void NaughtyWordCensor()
+        {
+            const string naughtyVersion = "He is a poopy head and darn idiot!";
+
+            var cleanVersion = string.Format(new NaughtyWordCensorFormatProvider(),
+                                                                 "Clean version: {0}",
+                                                                 naughtyVersion);
+
+            var cleanVersion2 = string.Format(new NaughtyWordCensorFormatProvider(),
+                                                     "Clean version: {0:xxx}, {1}",
+                                                     naughtyVersion,
+                                                     "some other darn string");
+
+            // WordyFormatProvider:  http://bit.ly/pscsformat 
+        }
+    }
+}
