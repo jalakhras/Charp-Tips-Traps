0830fb3aa1aaff7abf227f8f0ab23aa7685969e1
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-05-22 22:02:03 +0300

    Avoiding DateTime misparsing with culture-independent strings

---
+++ Demos4/RoundTripDateTimeParsingExample.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demos4
+{
+    [TestClass]
+    public class RoundTripDateTimeParsingExample
+    {
+
+        [TestMethod]
+        public void DateTimeExampleWithTimeZone()
+        {
+            var original = new DateTime(2000, 12, 1, 13, 30, 0, DateTimeKind.Local);
+
+            var safeRoundTripString = original.ToString("o");
+
+            var reparsed = DateTime.Parse(safeRoundTripString);
+
+            Debug.WriteLine(original);
+            Debug.WriteLine(safeRoundTripString);
+            Debug.WriteLine(reparsed);
+        }
+
+        [TestMethod]
+        public void DateTimeExampleWithoutTimeZone()
+        {
+            // defaults to DateTimeKind.Unspecified
+            var original = new DateTime(2000, 12, 1, 13, 30, 0);
+
+            var safeRoundTripString = original.ToString("o");
+
+            var reparsed = DateTime.Parse(safeRoundTripString);
+
+            Debug.WriteLine(original);
+            Debug.WriteLine(safeRoundTripString);
+            Debug.WriteLine(reparsed);
+        }
+
+        [TestMethod]
+        public void DateTimeExampleEnforcingRoundTripFormat()
+        {
+            string almostIso8601 = "2000/12/01T13:30:00.0000000+08:00";
+
+            var reparsed = DateTime.Parse(almostIso8601);
+
+            reparsed = DateTime.ParseExact(almostIso8601, "o", null);
+        }
+
+        [TestMethod]
+        public void DateTimeOffsetExample()
+        {
+            var original = new DateTimeOffset(2000, 12, 1, 13, 30, 0,
+            TimeSpan.FromHours(-8));
+
+            var safeRoundTripString = original.ToString("o");
+
+            var reparsed = DateTimeOffset.Parse(safeRoundTripString);
+
+            Debug.WriteLine(original);
+            Debug.WriteLine(safeRoundTripString);
+            Debug.WriteLine(reparsed);
+        }
+
+
+
+    }
+}
