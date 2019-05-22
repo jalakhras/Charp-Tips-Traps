733bf4e4fb2c8921a31650da9b7bcf363324b4b1
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-05-22 21:55:59 +0300

    Preventing ambiguous DateTime parsing

---
+++ Demos4/ParseExactDateTimeExample.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+
+namespace Demos4
+{
+    [TestClass]
+    public class ParseExactDateTimeExample
+    {
+        [TestMethod]
+        public void AmbiguousParse()
+        {
+            const string ambiguousDateString = "01/12/2000";
+
+            var d = DateTime.Parse(ambiguousDateString);
+
+            Debug.WriteLine(d.ToLongDateString());
+
+            d = DateTime.ParseExact(ambiguousDateString, "MM/dd/yyyy", null);
+
+            Debug.WriteLine(d.ToLongDateString());
+        }
+
+        [TestMethod]
+        public void StrictParse()
+        {
+            const string nonLocalFormat = "01-12-2000";
+
+            var d = DateTime.Parse(nonLocalFormat);
+
+            Debug.WriteLine(d.ToLongDateString());
+
+            d = DateTime.ParseExact(nonLocalFormat, "dd/MM/yyyy", null);
+        }
+
+    }
+}
