6f142a08963bd4c5ced4be1aa6a51cdafe8543e3
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-05-22 22:06:42 +0300

    Parsing DateTimes with the DateTimeStyles enumeration

---
+++ Demos4/DateTimeStylesExample.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Globalization;
+
+namespace Demos4
+{
+    [TestClass]
+    public class DateTimeStylesExample
+    {
+
+        [TestMethod]
+        public void UsefulDateTimeStyles()
+        {
+            DateTime d1 = DateTime.Parse("01/12/2000", null, DateTimeStyles.AssumeUniversal);
+
+            DateTime d2 = DateTime.Parse("01/12/2000", null, DateTimeStyles.AssumeLocal);
+
+            DateTime d3 = DateTime.Parse("13:30:00", null, DateTimeStyles.NoCurrentDateDefault);
+
+            DateTime d4 = DateTime.Parse("13:30:00", null, DateTimeStyles.None);
+        }
+
+    }
+}
