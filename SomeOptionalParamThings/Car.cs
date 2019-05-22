d29a7602eeb62d02f21788e372a977169bc0099d
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-05-22 22:20:07 +0300

    Binary compatibility with optional parameters

---
+++ SomeOptionalParamThings/Car.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SomeOptionalParamThings
+{
+    public class Car
+    {
+        private readonly string _color;
+        private readonly int _numberOfWheels;
+
+        public Car(string color, int numberOfWheels = 6, int windows = 4)
+        {
+            _color = color;
+            _numberOfWheels = numberOfWheels;
+        }
+
+
+        public override string ToString()
+        {
+            return string.Format("Color:{0}  Wheels:{1}", _color, _numberOfWheels);
+        }
+    }
+
+}
