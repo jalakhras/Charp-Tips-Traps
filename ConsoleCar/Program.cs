5817567263525edb7d7abbdbab86966a910ffc04
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2019-05-22 22:15:09 +0300

    as or cast?

---
+++ ConsoleCar/Program.cs
@@ -0,0 +1,21 @@
+﻿using SomeOptionalParamThings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Car("Blue");
+
+            Console.WriteLine(c);
+
+            Console.ReadLine();
+        }
+    }
+}
